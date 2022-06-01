7fdfe50d306b2017ec703e8fdd0e6128b0358ff6
kycjw <102590645+kycjw@users.noreply.github.com>
2022-06-01 16:37:08 +0900

    2 commit
    2 commit

--- js/main.js
+++ js/main.js
@@ -1,4 +1,10 @@
 $(window).ready(function(){
+    // source:  https://css-tricks.com/the-trick-to-viewport-units-on-mobile/
+    // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+    let vh = window.innerHeight * 0.01;
+    // Then we set the value in the --vh custom property to the root of the document
+    document.documentElement.style.setProperty("--vh", `${vh}px`);
+
     var portfolioList = [
         {
             title : "예일이퀍먼트",
